9ba280f9b00fd85c5c00a98905cd39fea621f7f2
izaquebsui <officialbsu42@gmail.com>
2025-06-22 17:15:53 -0300

    Corrigindo problemas de build para Vercel

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,11 @@ import App from "./App";
 import "./styles.css";
 
 const rootElement = document.getElementById("root");
+
+if (!rootElement) {
+  throw new Error("Elemento 'root' não encontrado no DOM.");
+}
+
 const root = createRoot(rootElement);
 
 root.render(
