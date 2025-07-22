07be5e8542efe3c3b7273872a09d81eed741e2ba
Izaquebsui <officialbsu42@gmail.com>
2025-07-22 15:20:10 -0300

    Corrigido tailwind.config.js e configs para deploy

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx"; // Está correto, pois App.jsx está no mesmo nível!
+import App from "./App.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,10 @@
-<<<<<<< HEAD
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx,html}",
   ],
-=======
-module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx,html}"],
->>>>>>> 05acf47988b71a656f400edc0c9003d4943e250b
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
